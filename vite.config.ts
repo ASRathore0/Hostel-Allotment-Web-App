9c03ef4b9fabd6a43cf43ff54acd4940cea76c61
ASRathore0 <asingh68416@gmail.com>
2025-05-31 14:23:11 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Hostel-Allotment-Web-App/', // 👈 Add this line with your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
